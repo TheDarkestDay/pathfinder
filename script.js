984eefec96cfec823488d4058e74aaaa22f228fe
PharoDeveloper <alexzund3r@yahoo.com>
2015-12-13 20:32:05 +0600

    GUI improvements

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ window.onload = function() {
         plusBtn = document.getElementById('enlarge'),
         minusBtn = document.getElementById('minimize'),
         maze = document.querySelector('.scrollable'),
+        goalModeBtn = document.getElementById('goalRadio'),
+        startModeBtn = document.getElementById('startRadio'),
+        wallModeBtn = document.getElementById('wallRadio'),
+        clearModeBtn = document.getElementById('clearRadio'),
         colsCount = 0,
         rowsCount = 0,
         mazeWidth = 500,
-        mazeHeight = 500;
+        mazeHeight = 500,
+        graph,
+        mode = 'start';
     
     createFieldBtn.addEventListener('click', function(evt) {
         evt.preventDefault();
@@ -24,19 +30,56 @@ window.onload = function() {
         cellWidth = mazeWidth/rowsCount;
         cellHeight = mazeHeight/colsCount;
         tbl = document.createElement('table');
+        graph = [];
         
         maze.innerHTML = '';
         for (var i=0;i<rowsCount;i++) {
             currRow = document.createElement('tr');
+            graph.push([]);
             for (var j=0;j<colsCount;j++) {
                 currCell = document.createElement('td');
+                currCell.addEventListener('click', function(evt) {
+                    evt.preventDefault();
+                    evt.target.className = "";
+                    switch(mode) {
+                        case 'goal':
+                            evt.target.classList.add('goal');
+                            break;
+                        case 'start':
+                            evt.target.classList.add('start');
+                            break;
+                        case 'wall':
+                            evt.target.classList.add('wall');
+                            break;
+                        default:
+                            break;
+                    };
+                });
                 currCell.style.width = cellWidth+'px';
                 currCell.style.height = cellHeight+'px';
                 currRow.appendChild(currCell);
+                graph[i].push(0);
             };
             tbl.appendChild(currRow);
         };
         maze.appendChild(tbl);
+        console.log(graph);
+    });
+    
+    goalModeBtn.addEventListener('click', function(evt) {
+        mode = 'goal';
+    });
+    
+    startModeBtn.addEventListener('click', function(evt) {
+        mode = 'start';
+    });
+    
+    wallModeBtn.addEventListener('click', function(evt) {
+        mode = 'wall';
+    });
+    
+    clearModeBtn.addEventListener('click', function(evt) {
+        mode = 'clear';
     });
     
     plusBtn.addEventListener('click', function(evt) {
